370ff7ee67768553ad8d265b2bbb0e29d3f55e50
SameC137 <samemichael1415@gmail.com>
2021-08-02 07:54:13 +0300

    Cml setup

---
+++ train.py
@@ -0,0 +1,28 @@
+import pickle
+import sklearn
+
+import sys, os
+
+from .scripts.CustomMaxImputer import CustomMaxImputer
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+try:
+    file = open("./models/model.pkl",'rb')
+    model = pickle.load(file)
+except:
+    print("Error Loading Model")
+
+try:
+    data=pd.read_csv("./data/val.csv")
+except:
+    print("Csv File Could not be read")
+
+
+y=data["Sales"]
+x=data.drop(["Sales","Customers","Date","Store"],inplace=False)
+prediction = model.predict(x)
+error=mean_squared_error(y,prediction)
+
+with open("metrics.txt", 'w') as outfile:
+        outfile.write("MeanSquaredError: " + str(error) + "\n")
